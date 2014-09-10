6935cb5de026eb39c3d52ae2d43efc6b4afc2a4a
Daniel Harrison <dan@foursquare.com>
2014-09-10 21:32:04 +0000

    shell out to pants in a background thread

--- PantsImportGen.py
+++ PantsImportGen.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import json
 import os
 import subprocess
+import threading
 
 
 class PantsImportInsertCommand(sublime_plugin.TextCommand):
@@ -21,26 +22,40 @@ class PantsImportInsertCommand(sublime_plugin.TextCommand):
     self.view.insert(edit, insertion_point, content)
 
 
+class PantsImportGenCall(threading.Thread):
+    def __init__(self, pwd, file_path, symbols):
+        self.pwd = pwd
+        self.file_path = file_path
+        self.symbols = symbols
+        threading.Thread.__init__(self)
+ 
+    def run(self):
+      command = ["./fs", "importgen", "--importgen-file=" + self.file_path]
+      for symbol in self.symbols:
+        command.append("--importgen-symbol=" + symbol)
+      self.detail = json.loads(subprocess.check_output(command, cwd=self.pwd).decode("UTF-8"))
+
+
 class PantsImportGenCommand(sublime_plugin.TextCommand):
 
   def find_pwd(self):
     pwd, _ = os.path.split(os.path.abspath(self.view.file_name()))
     while pwd:
-      print(pwd)
       if os.path.isfile(os.path.join(pwd, 'fs')):
         return pwd
       pwd, _ = os.path.split(pwd)
     # TODO(dan): Alert.
     print("Where is your pants!?")
 
-  def multi_select_callback(self, new_imports, ambiguous_new_import, ambiguous_new_imports, i):
+  def multi_select_callback(self, new_imports, already_imported_symbols, unknown_symbols, ambiguous_new_import, ambiguous_new_imports, i):
     if i != -1:
       new_imports.append(ambiguous_new_import[1][i])
     if len(ambiguous_new_imports) == 0:
+      self.view.set_status('pants_import_gen', 'Pants Import Gen [New=%d Unknown=%d Existing=%d]' % (len(new_imports), len(already_imported_symbols), len(unknown_symbols)))
       self.view.run_command("pants_import_insert", {"imports" : new_imports})
       return
     next_ambiguous_new_import = ambiguous_new_imports.pop(0)
-    callback = lambda i: self.multi_select_callback(new_imports, next_ambiguous_new_import, ambiguous_new_imports, i)
+    callback = lambda i: self.multi_select_callback(new_imports, already_imported_symbols, unknown_symbols, next_ambiguous_new_import, ambiguous_new_imports, i)
     sublime.set_timeout(lambda: sublime.active_window().show_quick_panel(next_ambiguous_new_import[1], callback), 10)
 
   def run(self, edit):
@@ -54,15 +69,15 @@ class PantsImportGenCommand(sublime_plugin.TextCommand):
         symbols.add(symbol)
 
     if len(symbols):
-      command = ["./fs", "importgen", "--importgen-file=" + self.view.file_name()]
-      for symbol in symbols:
-        command.append("--importgen-symbol=" + symbol)
-      detail = json.loads(subprocess.check_output(command, cwd=self.find_pwd()).decode("UTF-8"))
-    else:
-      detail = {}
+      thread = PantsImportGenCall(self.find_pwd(), self.view.file_name(), symbols)
+      thread.start()
+      self.handle_threads([thread], symbols)
 
+  def parse_imports_from_detail(self, symbols, detail):
     new_imports = []
     ambiguous_new_imports = []
+    already_imported_symbols = []
+    unknown_symbols = []
     for symbol in symbols:
       if symbol in detail:
         completions = detail[symbol]
@@ -70,12 +85,34 @@ class PantsImportGenCommand(sublime_plugin.TextCommand):
           if len(completions[0]):
             new_imports.append(completions[0])
           else:
-            # print("already have " + symbol)
-            pass
+            already_imported_symbols.append(symbol)
         elif len(completions) > 1:
           ambiguous_new_imports.append((symbol, completions))
         else:
-          # print("unknown " + symbol)
-          pass
-    self.multi_select_callback(new_imports, [], ambiguous_new_imports, -1)
+          unknown_symbols.append(symbol)
+    self.multi_select_callback(new_imports, already_imported_symbols, unknown_symbols, [], ambiguous_new_imports, -1)
 
+  # handle_threads courtesy of https://github.com/wbond/sublime_prefixr/blob/master/Prefixr.py
+  def handle_threads(self, threads, symbols, offset=0, i=0, dir=1):
+    next_threads = []
+    for thread in threads:
+      if thread.is_alive():
+        next_threads.append(thread)
+        continue
+      self.parse_imports_from_detail(symbols, thread.detail)
+    threads = next_threads
+
+    if len(threads):
+      # This animates a little activity indicator in the status area
+      before = i % 8
+      after = (7) - before
+      if not after:
+          dir = -1
+      if not before:
+          dir = 1
+      i += dir
+      self.view.set_status('pants_import_gen', 'Pants Import Gen [%s=%s]' % (' ' * before, ' ' * after))
+      sublime.set_timeout(lambda: self.handle_threads(threads, symbols, offset, i, dir), 100)
+      return
+
+    self.view.erase_status('pants_import_gen')
--- README.md
+++ README.md
@@ -7,8 +7,6 @@ Install using [Package Control](https://sublime.wbond.net/installation).
 
 TODOs
 ----
-- Shell out to pants in a background thread.
-- Update status bar with number of imports added.
 - Filter type parameters out from the list of symbols sent.
 - Get list of symbols from pants.
 - Add hooks to menu and Main and Context menus.
